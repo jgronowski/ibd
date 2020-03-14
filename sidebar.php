e1e3f2c6b788c7058f7261544d877f0ff099fd0c
jgronowski <jaroslaw@gronowski.net.pl>
2020-03-14 22:01:19 +0100

    Butify sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -8,21 +8,25 @@ $ostat = $ksiazki->pobierzBestsellery();
 
 <div class="col-md-2">
 	<h1>Bestsellery</h1>
-
-    <ul>
-        <?php foreach($ostat as $ks): ?>
-            <li class="list-group-item"
-                <center>
-                    <?php if(!empty($ks['zdjecie'])): ?>
-                        <a href="ksiazki.szczegoly.php?id=<?=$ks['id']?>"><img src="zdjecia/<?=$ks['zdjecie']?>" alt="<?=$ks['tytul']?>" class="img-thumbnail" /></a>
-                    <?php else: ?> brak zdjęcia
-                    <?php endif; ?>
-                </center>
+    <?php foreach($ostat as $ks): ?>
+    <div class="card flex-md-row mb-4 box-shadow h-md-250">
+        <div class="card-body d-flex flex-column align-items-start">
+            <h6 class="d-inline-block mb-2 text-primary"><?=$ks['tytul']?></h6>
+            <strong class="mb-1">
                 <a href="ksiazki.szczegoly.php?id=<?=$ks['id']?>">
-                    <p>
-                        <?=$ks['tytul']?>
-                    </p>
-                    <?=$ks['imie']?> <cite title="Source Title"><?=$ks['nazwisko']?></a></li>		<br>
-        <?php endforeach; ?>
-    </ul>
+
+                    <?=$ks['imie']?> <cite title="Source Title"><?=$ks['nazwisko']?>
+                </a>
+            </strong>
+            <div class="mb-1 text-muted"> <?=$ks['cena']?> PLN</div>
+            <?php if(!empty($ks['zdjecie'])): ?>
+                <a href="ksiazki.szczegoly.php?id=<?=$ks['id']?>"><img src="zdjecia/<?=$ks['zdjecie']?>" alt="<?=$ks['tytul']?>" class="img-thumbnail" /></a>
+            <?php else: ?>
+                <a href="ksiazki.szczegoly.php?id=<?=$ks['id']?>"><img src="zdjecia/noimage.jpg" alt="<?=$ks['tytul']?>" class="img-thumbnail" /></a>
+            <?php endif; ?>
+        </div>
+
+    </div>
+    <?php endforeach; ?>
+
 </div>
